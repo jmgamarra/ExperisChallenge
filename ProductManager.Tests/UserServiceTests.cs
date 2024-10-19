cc94ea38cb2a0adea8d7692f1d58076ac924faee
Jorge Gamarra <dev.net.jgamarra@gmail.com>
2024-10-19 11:05:48 -0500

    primer test unitario para User, el cual fallara

---
+++ ProductManager.Tests/UserServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManager.Tests
+{
+    public class UserServiceTests
+    {
+        [Fact]
+        public void CreateUser_ShouldReturnTrue_WhenUserIsValid()
+        {
+            // Arrange
+            var userService = new UserService(); // No hay inyección ni repositorios aún
+            var user = new User { UserName = "testuser", Password = "password123" };
+
+            // Act
+            var result = userService.Create(user);
+
+            // Assert
+            Assert.True(result); // Este test fallará 
+        }
+    }
+}
